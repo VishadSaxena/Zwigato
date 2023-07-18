51e79facd8e020c4aca47cca085c9b437d471f99
Vishad Saxena <vishad1710@gmail.com>
2023-07-18 20:45:32 +0530

    Update Bod,Shimmer Component
    Added Shimmer UI for initial rendering

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,32 @@
 import { useState, useEffect } from "react";
 import RestCard from "./RestCard";
 import resList from "../utils/mockData";
-
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
    const [ListOfRest, setListOfRest] = useState([]);
 
    useEffect(() => {
+
     fetchData();
-   },[]);
+
+   }, []);
 
    const fetchData = async () => {
+    
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.834702025354822&lng=80.88480837643147&page_type=DESKTOP_WEB_LISTING");
 
     const json = await data.json();
 
-    console.log(json);
-    setListOfRest(json?.data?.cards[2]?.data?.data?.cards);
-   }
+    setListOfRest(json.data.cards[2].data.data.cards);
+
+    }
+
+    if(ListOfRest.length === 0){
+        return < Shimmer />
+    }
+
         return(
         <div className="body">
             <div>
