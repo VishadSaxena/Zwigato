c0fa038ec0540e5b7d4865972284d7dba46a92c2
Vishad Saxena <vishad1710@gmail.com>
2023-07-18 23:18:58 +0530

    Update; Heade with Login Btn
    Added useState Variable to change Login/Logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+
+    let log_val = "";
+
+    const [logValue, setLogValue] = useState('Login');
+
     return (
       <div className="header">
         <img className="logo"
@@ -11,6 +17,13 @@ const Header = () => {
           <li>Home</li>
           <li>About Us</li>
           <li>Cart</li>
+          <button className="btnLogin" onClick={()=>{
+                logValue === "Login" ?  setLogValue("Logout") : setLogValue("Login");
+                // log_val = "Logout";
+                // setLogValue(log_val);
+                // console.log(log_val);
+          }}>
+           {logValue} </button>
         </ul>
       </div>
     );
