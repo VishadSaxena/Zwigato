d0086ee1b26f812744690eecf91ac5177a720de4
Vishad Saxena <vishad1710@gmail.com>
2023-08-01 22:01:36 +0530

    added: State variables in Class based components
    using this.state object added and updated state variables in class based components

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const UserCard = (props) => {
+
+    const [count,setCount] = useState(0);
 
     const {name,Location} = props;
 
@@ -6,6 +10,10 @@ const UserCard = (props) => {
         <div className="user-card">
 
             <h1>Name : {name}</h1>
+            <button className="btn-modify" onClick={()=>{
+                setCount(count+1);
+            }}> Count </button>
+            <h2> Count : {count}</h2>
             <h2>Location : {Location}</h2>
             <h3>
                 Email: vishad1710@gmail.com
--- src/components/UserCardClass.js
+++ src/components/UserCardClass.js
@@ -5,6 +5,9 @@ class UserCardClass extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            count:0,
+        };
         console.log(props);
     }
     render(){
@@ -14,6 +17,12 @@ class UserCardClass extends React.Component {
             <div className="user-card">
 
                 <h1>Name : {name}</h1>
+                <button onClick={ () => {
+                    this.setState({
+                        count : this.state.count + 1,
+                    });
+                }}> Count</button>
+                <h2>Count : {this.state.count}</h2>
                 <h2>Location : {location}</h2>
                 <h3>
                     Email: vishad1710@gmail.com
